91862e784dec91126069657739113e063c934068
Emma Sudds <emmasudds@live.co.uk>
2019-01-10 22:04:52 +0000

    Added controller method to handle the submission of the contact form. Added jquery to use ajax to submit the contact form. Ajax method cannot find the controller method at this point.

--- StreetEats/Controllers/WeddingsController.cs
+++ StreetEats/Controllers/WeddingsController.cs
@@ -5,11 +5,24 @@ using System.Web;
 using System.Web.Mvc;
 using StreetEats.Models;
 using System.IO;
+using System.Net.Mail;
+using System.Net;
 
 namespace StreetEats.Controllers
 {
     public class WeddingsController : Controller
     {
+        public static string GmailUsername { get; set; }
+        public static string GmailPassword { get; set; }
+        public static string GmailHost { get; set; }
+        public static int GmailPort { get; set; }
+        public static bool GmailSSL { get; set; }
+
+        public string ToEmail { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public bool IsHtml { get; set; }
+
         // GET: Wedding
         public ActionResult Index()
         {
@@ -46,6 +59,40 @@ namespace StreetEats.Controllers
             };
 
             return View(weddingInfo);
+        }
+
+        [HttpPost]
+        public ActionResult AjaxContactForm(Contact model)
+        {
+            GmailUsername = "emmasudsey@gmail.com";
+            GmailPassword = "Deadpool2019!";
+            ToEmail = "emmasudds@live.co.uk";
+            Subject = "Wedding";
+            Body = model.message + "<br/" + "From: " + model.email;
+            IsHtml = true;
+            Send();
+            return null;
+        }
+
+        public void Send()
+        {
+            GmailPort = 25;
+            GmailSSL = true;
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = GmailHost;
+            smtp.Port = GmailPort;
+            smtp.EnableSsl = GmailSSL;
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new NetworkCredential(GmailUsername, GmailPassword);
+
+            using (var message = new MailMessage(GmailUsername, ToEmail))
+            {
+                message.Subject = Subject;
+                message.Body = Body;
+                message.IsBodyHtml = IsHtml;
+                smtp.Send(message);
+            }
         }
     }
 }
